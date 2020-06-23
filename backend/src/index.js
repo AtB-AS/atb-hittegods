ab06af6b8c700623603f3dfe24c4d9f9ec5f68d2
gajaen <gajaen@icloud.com>
2020-06-23 16:34:59 +0200

    Backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/index.js
+var express = require("express");
+var port = process.env.PORT || 5000;
+var app = express();
+app.listen(port);
---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+// src/index.js
+import express = require('express');
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use("/",(req,res)=>{
+    res.json("Hello World!");  
+});
+
+app.listen(port);

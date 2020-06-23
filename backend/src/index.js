b75c8aa52951fc6f656b4db72b6c393a9cdf3b40
Terje Røstum <terje.rostum@kantega.no>
2020-06-23 23:43:25 +0200

    Refactor and move backend into packages. Enable running both backend and frontend from root. Remove duplicate lock files. Target backend build output to dist folder.

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.js
-var express = require("express");
-var port = process.env.PORT || 5000;
-var app = express();
-app.listen(port);
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,10 @@
-// src/index.js
 import express = require('express');
-
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use("/",(req,res)=>{
-    res.json("Hello World!");  
+    res.json("Hello World!");
 });
 
 app.listen(port);

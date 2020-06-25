02392b69fe0b0c0b568281e865351c9c325fa17b
Renate <renate.kje@gmail.com>
2020-06-25 13:02:10 +0200

    Grid

--- packages/grizzly/src/App.tsx
+++ packages/grizzly/src/App.tsx
@@ -5,6 +5,7 @@ import categoryBags from "./components/img/categoryBags.png";
 import categoryClothing from "./components/img/categoryClothing.png";
 import categoryElectronics from "./components/img/categoryElectronics.png";
 import categoryPersonalEffects from "./components/img/PersonalEffects.png";
+import logo from "./components/img/AtB_gra-kopi.png";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -15,39 +16,47 @@ function App() {
   return (
     <Col className="App">
       <header className="App-header">
+
+
           <Container>
-              <form action="">
+              <Row>
+                  <Col sm={1}>
+                      <img src={logo} width="200px"/>
+                  </Col>
+                  <Col sm={11}></Col>
+              </Row>
+              <Col >
+                  <h1>Kategorier</h1>
+              </Col>
                   <Row>
-                      <Col> 
-                          <label>Bag, veske og sekk </label>
+
+                      <Col>
+                          <h2 id="kategori">Bag, veske og sekk </h2>
                           <button type="submit" value="categoryBags">
                               <img src={categoryBags} />
                           </button>
                       </Col>
                       <Col className="col">
-                          <label>Klær</label>
+                          <h2 id="kategori">Klær</h2>
                           <button type="submit" value="categoryClothing">
                               <img src={categoryClothing} />
                           </button>
                       </Col>
-
                   </Row>
                   <Row>
                       <Col>
-                          <label>Elektronikk </label>
+                          <h2 id="kategori">Elektronikk </h2>
                           <button type="submit" value="categoryElectronics">
                               <img src={categoryElectronics} />
                           </button>
                       </Col>
                       <Col>
-                          <label>Personlige effekter </label>
+                          <h2 id="kategori">Personlige effekter </h2>
                           <button type="submit" value="categoryPersonalEffects">
                               <img src={categoryPersonalEffects} />
                           </button>
                       </Col>
                   </Row>
-
-              </form>
           </Container>
           <BjornesnuteButton/>
 

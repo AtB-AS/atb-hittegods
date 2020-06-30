567b785b8a18f97289afc4f75cf13bbbd885271d
Renate <renate.kje@gmail.com>
2020-06-30 09:11:22 +0200

    .

--- packages/grizzly/src/MainCategory.tsx
+++ packages/grizzly/src/MainCategory.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
+import CategoryClothing from "./components/img/categoryClothing.png";
 
 function MainCategory() {
   return (
     <Grid container spacing={3}>
       <Grid item sm={6}>
-        <Paper>HEI</Paper>
+        <Paper>{CategoryClothing}</Paper>
       </Grid>
     </Grid>
   );

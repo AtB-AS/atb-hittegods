f3324acd58c11780ae0a02834fdff83a18ffe9c5
gajaen <gajaen@icloud.com>
2020-07-03 11:12:16 +0200

    Date for today and yesterday and some styling

--- packages/grizzly/src/MissingDate.tsx
+++ packages/grizzly/src/MissingDate.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, LinearProgress } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
 
-import DateFnsUtils from "@date-io/date-fns";
 import Button from "@material-ui/core/Button";
 
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 type Props = {
   onDateSelect: (date: string) => void;
@@ -25,7 +26,15 @@ function MissingDate(props: Props) {
       textField: {
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
-        width: 200,
+        width: "100%",
+      },
+      btn: {
+        border: 0,
+        borderRadius: 3,
+        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .3)",
+        height: "60px",
+        padding: "0 30px",
+        width: "100%",
       },
     })
   );
@@ -35,15 +44,55 @@ function MissingDate(props: Props) {
     props.onDateSelect(date);
   }
 
+  function onSubmitToday() {
+    let date: string;
+    // @ts-ignore
+    date = new Date().toJSON().split("T")[0];
+    console.log(date);
+    props.onDateSelect(date);
+  }
+
+  function onSubmitYesterday() {
+    let yesterday = ((d) => new Date(d.setDate(d.getDate() - 1)))(new Date())
+      .toJSON()
+      .split("T")[0];
+    console.log(yesterday);
+    props.onDateSelect(yesterday);
+  }
+
   return (
-    <Grid container spacing={3}>
-      <Grid item sm={6}>
-        <h1>Når mistet du gjenstanden din?</h1>
-        <form className={classes.container} noValidate>
+    <div>
+      <Box mt={6}>
+        <h2>Når mistet du dette?</h2>
+      </Box>
+      <Box mt={3}>
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          onClick={onSubmitToday}
+          className={classes.btn}
+        >
+          I dag
+        </Button>
+      </Box>
+      <Box mt={3}>
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          onClick={onSubmitYesterday}
+          className={classes.btn}
+        >
+          I går
+        </Button>
+      </Box>
+      <Box mt={3}>
+        <form noValidate>
           <TextField
             label="Dato"
             type="date"
-            defaultValue="2020/07/02"
+            defaultValue={new Date()}
             className={classes.textField}
             InputLabelProps={{
               shrink: true,
@@ -52,7 +101,8 @@ function MissingDate(props: Props) {
             onChange={(event) => setDate(event.target.value)}
           />
         </form>
-
+      </Box>
+      <Box mt={3}>
         <Button
           color="primary"
           variant="contained"
@@ -61,8 +111,8 @@ function MissingDate(props: Props) {
         >
           Neste
         </Button>
-      </Grid>
-    </Grid>
+      </Box>
+    </div>
   );
 }
 

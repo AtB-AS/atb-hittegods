0e310f8f0f18a51c690107a8adb9de00e1ebae67
Renate <renate.kje@gmail.com>
2020-07-07 16:33:50 +0200

    Admin henvendelse setUp

---
+++ packages/grizzly/src/admin/Henvendelse.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { log } from "util";
+import { Grid } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+
+type Props = {
+  match: {
+    params: {
+      id: string;
+    };
+  };
+};
+
+type Henvendelsen = {
+  id: number;
+  subcategory: string;
+  description: string;
+  matchCount: number;
+  newMatchCount: number;
+};
+
+function Henvendelse(props: Props) {
+  const [henvendelse, setHenvendelse] = useState<Henvendelsen | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState(false);
+
+  const parameters = {
+    id: props.match.params.id,
+  };
+
+  const queryString = Object.entries(parameters)
+    .map(([key, val]) => `${key}=${val}`)
+    .join("&");
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/admin/lostDetails/" + "?" + queryString)
+      .then((response) => {
+        if (response.status === 401) {
+        } else {
+          return response.json();
+        }
+      })
+      .then((jsonData) => {
+        setHenvendelse(jsonData.data);
+        setLoading(false);
+        console.log(jsonData);
+      })
+      .catch(() => {
+        setError(true);
+      });
+  }, [props.match.params.id]);
+
+  if (error) {
+    return <p>"Noe gikk galt :("</p>;
+  }
+
+  if (isLoading) {
+    return <p>"Laster..."</p>;
+  }
+
+  return (
+    <div>
+      HEI {props.match.params.id}
+      {console.log(henvendelse?.subcategory)}
+      <Grid container>
+        <Paper>
+          <Grid item md={12}>
+            <p> ID, subcat, merke og dato</p>
+          </Grid>
+          <Grid item md={6}>
+            <h3>Innsender:</h3>
+            <p>Ola Hansen</p>
+            <p>90807060</p>
+            <p>email@email.com</p>
+            <h3>{henvendelse?.subcategory}</h3>
+            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+          </Grid>
+          <Grid item md={6}>
+            <h3>BILDE:)</h3>
+            <p>Linje: 3</p>
+          </Grid>
+        </Paper>
+
+        <Paper>
+          <Grid item md={12}>
+            Her kommer mulige funn!
+          </Grid>
+        </Paper>
+      </Grid>
+    </div>
+  );
+}
+
+export default Henvendelse;
--- packages/grizzly/src/admin/Henvendelser.tsx
+++ packages/grizzly/src/admin/Henvendelser.tsx
@@ -1,53 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TableContainer } from "@material-ui/core";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
+import { Route, useHistory } from "react-router";
+import Henvendelse from "./Henvendelse";
+import { log } from "util";
 
-const data = [
-  {
-    id: 1,
-    name: "Per",
-    subCat: "Nøkler",
-    description: "Gule nøkler",
-    inventoryTotal: 3,
-    inventoryNew: 1,
+type Henvendelse = {
+  id: number;
+  subcategory: string;
+  description: string;
+  matchCount: number;
+  newMatchCount: number;
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
   },
-];
+  container: {
+    maxHeight: "90vh",
+  },
+});
 
 function Henvendelser() {
+  const classes = useStyles();
+  const history = useHistory();
+  const [henvendelser, setHenvendelser] = useState<Henvendelse[]>([]);
+
+  const params = {
+    from: 0,
+    to: 25,
+  };
+
+  const queryString = Object.entries(params)
+    .map(([key, val]) => `${key}=${val}`)
+    .join("&");
+
+  useEffect(() => {
+    fetch("/api/admin/lost" + "?" + queryString)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setHenvendelser(jsonData.data.items);
+      })
+      .catch((error) => console.log(error + "Oh no:("));
+  }, []);
+
+  function clickedRowItem(id: number) {
+    history.push("/admin/henvendelser/" + id);
+  }
+
   return (
     <div>
-      <TableContainer>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell>Navn</TableCell>
-              <TableCell>Underkategori</TableCell>
-              <TableCell>Beskrivelse</TableCell>
-              <TableCell>Antall på lager</TableCell>
-              <TableCell>Nye funn</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row) => {
-              return (
+      <Grid container>
+        <Grid item md={7}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
                 <TableRow>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.subCat}</TableCell>
-                  <TableCell>{row.description}</TableCell>
-                  <TableCell>{row.inventoryTotal}</TableCell>
-                  <TableCell>{row.inventoryNew}</TableCell>
+                  <TableCell>Id</TableCell>
+                  <TableCell>Navn</TableCell>
+                  <TableCell>Underkategori</TableCell>
+                  <TableCell>Beskrivelse</TableCell>
+                  <TableCell>Antall på lager</TableCell>
+                  <TableCell>Nye funn</TableCell>
                 </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              </TableHead>
+              <TableBody>
+                {henvendelser.map((item) => {
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => clickedRowItem(item.id)}
+                    >
+                      <TableCell>{item.id}</TableCell>
+                      <TableCell>Per</TableCell>
+                      <TableCell>{item.subcategory}</TableCell>
+                      <TableCell>{item.description}</TableCell>
+                      <TableCell>{item.matchCount}</TableCell>
+                      <TableCell>{item.newMatchCount}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item md={5}>
+          <Route path="/admin/henvendelser/:id" component={Henvendelse} />
+        </Grid>
+      </Grid>
     </div>
   );
 }

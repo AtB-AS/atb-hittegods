338d2d908264e53898e00508c466973e6ceb1317
Renate <renate.kje@gmail.com>
2020-07-13 14:58:09 +0200

    removed unused code

--- packages/grizzly/src/Location.tsx
+++ packages/grizzly/src/Location.tsx
@@ -35,8 +35,6 @@ function Location(props: Props) {
   const [lines, setLines] = useState([]);
   const [error, setError] = useState(false);
   const [isloading, setLoading] = useState(true);
-  // status = 0 (no show), status = 1 (show yes), status = 2 (show no)
-  const [status, setStatus] = useState(0);
   const [line, setLine] = useState("");
 
   useEffect(() => {
@@ -62,41 +60,6 @@ function Location(props: Props) {
   function unknownLineButtonHandler() {
     setLine("");
     props.onLocationSelect(line);
-  }
-
-  function radioHandler(status: number) {
-    return setStatus(status);
-  }
-
-  function displayNoContent() {
-    props.onLocationSelect("");
-  }
-
-  function displayYesContent() {
-    return (
-      <div>
-        <Box>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <h3 className={styles.heading}>Linje</h3>
-                {console.log(lines)}
-                <select name="line" onChange={(e) => setLine(e.target.value)}>
-                  {lines.map((item) => {
-                    return <option key={item}>{item}</option>;
-                  })}
-                </select>
-              </Grid>
-              <Grid item xs={12}>
-                <Button color="primary" type="submit" variant="contained">
-                  Neste
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
-      </div>
-    );
   }
 
   if (error) {
@@ -148,23 +111,6 @@ function Location(props: Props) {
             </Grid>
           </form>
         </Box>
-
-        <label>Ja</label>
-        <input
-          type="radio"
-          name="yesNo"
-          checked={status === 1}
-          onChange={(event) => radioHandler(1)}
-        />
-        <label>Nei</label>
-        <input
-          type="radio"
-          name="yesNo"
-          checked={status === 2}
-          onChange={(event) => radioHandler(2)}
-        />
-        {status === 1 && displayYesContent()}
-        {status === 2 && displayNoContent()}
       </div>
     </div>
   );

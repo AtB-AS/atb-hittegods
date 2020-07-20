ceed1e665f5e9a00b90c9949ba7ac5950dcf7733
Renate <renate.kje@gmail.com>
2020-07-20 15:51:57 +0200

    Next button Characteristics done

--- packages/grizzly/src/Characteristics.tsx
+++ packages/grizzly/src/Characteristics.tsx
@@ -19,6 +19,7 @@ import Radio from "@material-ui/core/Radio";
 import FormControl from "@material-ui/core/FormControl";
 import ColorSelect from "./ColorSelect";
 import InputLabel from "@material-ui/core/InputLabel";
+import NextBtn from "./components/NextBtn";
 
 type Props = {
   onCharacteristicsSelect: (characteristics: Characteristics) => void;
@@ -38,6 +39,9 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     textfield: {
       display: "flex",
+    },
+    button: {
+      float: "right",
     },
   })
 );
@@ -91,14 +95,9 @@ function Characteristics(props: Props, characteristics: Characteristics) {
             />
           </Grid>
           <Grid item xs={12}>
-            <Button
-              color="primary"
-              variant="contained"
-              type="submit"
-              onClick={onSubmit}
-            >
-              Neste
-            </Button>
+            <div className={styles.button}>
+              <NextBtn onClick={onSubmit} />
+            </div>
           </Grid>
         </Grid>
       </Box>

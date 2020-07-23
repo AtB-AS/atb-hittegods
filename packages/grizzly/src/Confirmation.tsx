6ccde599ca15535f5dff1db44cd256cb75a5ca91
Terje Røstum <terje.rostum@kantega.no>
2020-07-23 11:52:33 +0200

    Bekreftelsesside som tar høyde for hvillken dag en gjenstand ble mistet

--- packages/grizzly/src/Confirmation.tsx
+++ packages/grizzly/src/Confirmation.tsx
@@ -71,50 +71,113 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const MailStep = () => {
+  const styles = useStyles();
+  return (
+    <div className={styles.step}>
+      <div className={styles.stepContent}>
+        <LetterImage />
+        <p>Vi sender deg beskjed på e-post om vi finner noe.</p>
+      </div>
+    </div>
+  );
+};
+
+const LostToday = () => {
+  const styles = useStyles();
+  return (
+    <div>
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <BusImage />
+          <p>
+            Bussjåføren vil gå gjennom bussen på endestoppet for å se om noe er
+            gjenglemt.
+          </p>
+        </div>
+        <LineImage1 className={styles.stepLine1} />
+      </div>
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <PostmanImage />
+          <p>Alt som blir funner vil bli levert inn etter endt skift.</p>
+        </div>
+        <LineImage2 className={styles.stepLine2} />
+      </div>
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <StorageImage />
+          <p>Slik at vi har det på lager hos oss i AtB i morgen kl 12.</p>
+        </div>
+        <LineImage3 className={styles.stepLine3} />
+      </div>
+      <MailStep />
+    </div>
+  );
+};
+
+const LostYesterday = () => {
+  const styles = useStyles();
+  return (
+    <div>
+      <div className={styles.step} />
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <PostmanImage />
+          <p>
+            Bussjåføren leverte inn det som ble funnet i bussen i går kveld.
+          </p>
+        </div>
+        <LineImage2 className={styles.stepLine2} />
+      </div>
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <StorageImage />
+          <p>Slik at vi har det på lager hos oss i AtB i dag kl 12.</p>
+        </div>
+        <LineImage3 className={styles.stepLine3} />
+      </div>
+      <MailStep />
+    </div>
+  );
+};
+
+const LostEarlier = () => {
+  const styles = useStyles();
+  return (
+    <div>
+      <div className={styles.step}>
+        <div className={styles.stepContent}>
+          <StorageImage />
+          <p>Vi skal sjekke i det vi har fått inn.</p>
+        </div>
+        <LineImage2 className={styles.stepLine2} />
+      </div>
+      <MailStep />
+    </div>
+  );
+};
+
 function Confirmation(props: Props) {
   const styles = useStyles();
+
+  const today = moment();
   const lostDate = moment(props.lostDate, "YYYY-MM-DD");
+  const daysSinceItemWasLost = today.diff(lostDate, "days");
 
   return (
     <div>
       <Box mt={4} mb={6}>
         <h2 className="h4">Vi er på saken!</h2>
         <p className={styles.lead}>
-          Vi skal lete for deg, men det kan ta litt tid før vi får det inn.
+          Du har nå gjort ditt, og vi skal ta over. Vi skal lete for deg, men
+          det kan ta litt tid før vi får det inn.
         </p>
+        <p>Du vil få en oppsumering sendt til {props.email}</p>
       </Box>
-      <div>
-        <div className={styles.step}>
-          <div className={styles.stepContent}>
-            <BusImage />
-            <p>
-              Bussjåføren vil gå gjennom bussen på endestoppet for å se om noe
-              er gjenglemt.
-            </p>
-          </div>
-          <LineImage1 className={styles.stepLine1} />
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepContent}>
-            <PostmanImage />
-            <p>Han leverer inn alt han finner i kveld</p>
-          </div>
-          <LineImage2 className={styles.stepLine2} />
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepContent}>
-            <StorageImage />
-            <p>Slik at vi har det på lager i morgen kl 12.</p>
-          </div>
-          <LineImage3 className={styles.stepLine3} />
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepContent}>
-            <LetterImage />
-            <p>Vi vil sende deg oppdateringer underveis.</p>
-          </div>
-        </div>
-      </div>
+      {daysSinceItemWasLost === 0 && <LostToday />}
+      {daysSinceItemWasLost === 1 && <LostYesterday />}
+      {daysSinceItemWasLost > 1 && <LostEarlier />}
     </div>
   );
 }
